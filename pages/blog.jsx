a83ecece163ad72b3911a9e952613004b370f2c1
Mehedi Pathan <sma.mehedi@gmai.com>
2023-08-13 21:46:09 +0600

    header and some styles updated v_20

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,32 +1,83 @@
-import '../styles/global.css';
-import Link from 'next/link';
-import Header from '../components/Header';
-import styles from '../styles/blog.module.css';
-import fadeIn from '../styles/transitions.module.css';
+import { useState } from "react";
+import "../styles/global.css";
+import Link from "next/link";
+import Header from "../components/Header";
+import styles from "../styles/blog.module.css";
+import fadeIn from "../styles/transitions.module.css";
+import ReactPaginate from "react-paginate";
 
-import blogData from '../data/blogData.json';
+import blogData from "../data/blogData.json";
 
 const Blog = () => {
-  return (
-    <div className={`${styles.pageContainer} ${fadeIn['fade-in']}`}>
-      <Header />
-      <div className={styles.content}>
-        <h1 className={styles.title}>Blog Posts</h1>
-        <div className={styles.postList}>
-          {blogData.map((post) => (
-            <Link href={`/${post.id}`} key={post.id} passHref>
-              <div className={styles['post-link']}>{post.title}</div>
-            </Link>
-          ))}
+    const postsPerPage = 5; // Number of posts per page
+    const [currentPage, setCurrentPage] = useState(0);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handlePageChange = (selectedPage) => {
+        setCurrentPage(selectedPage.selected);
+    };
+
+    const startIndex = currentPage * postsPerPage;
+    const endIndex = startIndex + postsPerPage;
+
+    // Filter posts based on search term
+    const filteredPosts = blogData.filter((post) =>
+        post.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const currentPosts = filteredPosts.slice(startIndex, endIndex);
+
+    const totalPages = Math.ceil(filteredPosts.length / postsPerPage);
+
+    return (
+        <div className={`${styles.pageContainer} ${fadeIn["fade-in"]}`}>
+            <Header />
+            <div className={styles.content}>
+                <h1 className={styles.title}>Blog Posts</h1>
+                <div className={styles.searchBar}>
+                    <input
+                        type='text'
+                        placeholder='Search posts...'
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className={styles.searchInput}
+                    />
+                    <button className={styles.searchButton}>Search</button>
+                </div>
+                <div className={styles.postList}>
+                    {currentPosts.map((post) => (
+                        <Link href={`/${post.id}`} key={post.id} passHref>
+                            <div className={styles["post-link"]}>
+                                {post.title}
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+                <div className={styles.pagination}>
+                    <ReactPaginate
+                        previousLabel={"Previous"}
+                        nextLabel={"Next"}
+                        breakLabel={"..."}
+                        pageCount={totalPages}
+                        marginPagesDisplayed={1}
+                        pageRangeDisplayed={3}
+                        onPageChange={handlePageChange}
+                        containerClassName={`${styles.paginationContainer} ${styles.paginationContainerCustom}`}
+                        pageClassName={styles.paginationPage}
+                        previousLinkClassName={styles.paginationLink}
+                        nextLinkClassName={styles.paginationLink}
+                        disabledClassName={styles.paginationDisabled}
+                        activeClassName={styles.paginationActive}
+                    />
+                </div>
+                <div className={styles.backButton}>
+                    <Link href='/' passHref>
+                        <div className={styles["back-link"]}>Back to Home</div>
+                    </Link>
+                </div>
+            </div>
         </div>
-        <div className={styles.backButton}>
-          <Link href="/" passHref>
-            <div className={styles['back-link']}>Back to Home</div>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Blog;
